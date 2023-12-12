1bad81a1bda0528faaead931951dd65559a0689d
Adam Purcell <apurcell88@gmail.com>
2023-12-12 14:59:59 -0600

    Added additional content and styling to home route.

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,16 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
-      {/* <h2>Dashboard</h2> */}
+    <main className="home--container">
+      <div className="blah">
+        <h1 className="home--title">Browse Thousands Of Movie Trailers</h1>
+        <p className="home--description">Information also includes what it is   about, rating and release date.</p>
+        <p className="home--description">Ready to Search? Go ahead and type below.</p>
+      </div>
+      <form className="home--search-form">
+        <input type="text" id="search" />
+        <button type="submit">Search</button>
+      </form>
     </main>
   )
 }
